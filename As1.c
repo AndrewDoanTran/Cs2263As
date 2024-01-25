63414d56bd6126e58dc0b9c359fa1bfa2d2520c3
AndrewDoanTran <155690892+AndrewDoanTran@users.noreply.github.com>
2024-01-25 19:37:30 -0400

    Add files via upload

--- As1.c
+++ As1.c
@@ -10,19 +10,71 @@ void printArr(char a[], int n)
 }
 
 void printReversed(int n)
-{
-    while ()
+{   int a, digit;
+    while (n > 0)
+    {
+        a = n%10;
+        digit = a + '0';
+        putchar(digit);
+        n = n/10;
+    }
 }
 
+int convertInt (char a[], int n)
+{   
+    int ans;
+
+    for(int i = n-1; i >= 0; i--)
+    {
+      
+    }
+    int T = ans/10;
+
+    // printf("%d", T);
+    printf("%d", ans);
+}
+
+int addReversedInt(char a[], int n, char b[], int m) 
+{
+    int firstNum = convertInt(a,n);
+    int secondNum = convertInt(b,m);
+    int ans;
+    return ans = firstNum + secondNum;
+
+}
 
 int main()
 {
     char a[] = {'H', 'e','l', 'l', 'o',' ', 'W','o','r','l','d'};
     int n = sizeof(a);
 
-    printArr(a,n);
+    // printArr(a,n);
     
-    printReversed(09072004);
+    // printf("\n");
+
+    // printReversed(321);
+
+    // printf("\n");
+
+    // printReversed(9072004);
+    
+    printf("\n");
+
+    char intArray[] = {'3','2','1'};
+    n = sizeof(intArray);
+    
+    convertInt(intArray, n);
+
+    printf("\n");
+
+    char intArray2[] = {'6','5','4'};
+    int m = sizeof(intArray2);
+
+    // addReversedInt(intArray,n,intArray2,m);
+
+
+
+
     
     printf("\n");
 
