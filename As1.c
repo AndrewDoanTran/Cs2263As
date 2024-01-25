68d5897b3027c8dfe678556ecae53c33c2ce5b8f
AndrewDoanTran <155690892+AndrewDoanTran@users.noreply.github.com>
2024-01-25 15:37:19 -0400

    Add files via upload

---
+++ As1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void printArr(char a[], int n)
+{   
+    int i;
+    for(int i = 0; i < n; i++)
+    {
+        putchar(a[i]);
+    }
+}
+
+void printReversed(int n)
+{
+    while ()
+}
+
+
+int main()
+{
+    char a[] = {'H', 'e','l', 'l', 'o',' ', 'W','o','r','l','d'};
+    int n = sizeof(a);
+
+    printArr(a,n);
+    
+    printReversed(09072004);
+    
+    printf("\n");
+
+    return 0;
+}
